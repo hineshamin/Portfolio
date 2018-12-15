3e721bb102557163dd5feb2c193fd2a57ecfb34c
Hinesh Amin <hineshamin01@gmail.com>
2018-12-15 11:14:24 -0800

    Fixing chatra secret

---
+++ public/chatraSetup.js
@@ -0,0 +1,10 @@
+window.ChatraSetup = {
+  chatWidth: 400,
+  chatHeight: 500,
+  colors: {
+    buttonText: '#43e0ff',
+    buttonBg: '#565656',
+    clientBubbleBg: '#00bd7b',
+    agentBubbleBg: '#565656'
+  }
+};
