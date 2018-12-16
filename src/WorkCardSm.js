569f9d7c5472d4aba72a7ef9f42cd844a05ae4c2
Hinesh Amin <hineshamin01@gmail.com>
2018-12-15 22:29:16 -0800

    Fixing margins

--- src/WorkCardSm.js
+++ src/WorkCardSm.js
@@ -6,17 +6,15 @@ class WorkCardSm extends Component {
   render() {
     return (
       <div className="row WorkCardSm">
-        <div className="col-1" />
         <Link
           to={`/work/${this.props.app}`}
-          className={`col-10 WorkCardSm-image ${this.props.app}`}
+          className={`col-12 WorkCardSm-image ${this.props.app}`}
         >
           <div>
             <h2 className="light">{this.props.title}</h2>
             <h3 className="light">{this.props.tech.join(' • ')}</h3>
           </div>
         </Link>
-        <div className="col-1" />
       </div>
     );
   }
