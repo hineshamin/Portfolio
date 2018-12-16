0c810cafaf55683200fcb110922642bc0bb4799e
Hinesh Amin <hineshamin01@gmail.com>
2018-12-16 11:15:12 -0800

    Adding work text

--- src/WorkInfo.js
+++ src/WorkInfo.js
@@ -6,10 +6,15 @@ class WorkInfo extends Component {
     return (
       <div className="row WorkInfo">
         <div className="col-1" />
-        <div className="col-10 px-0">
+        <div className="col-10 col-sm-6 col-xl-4 px-0">
           <h1 className="active" id="BigHeader">
             Work
           </h1>
+          <h3 className="light" id="subtext">
+            Below you can find projects that I have completed. Many of them have
+            github links to check out the code as well as external site links to
+            see a live hosted version.
+          </h3>
         </div>
         <div className="col-1" />
       </div>
