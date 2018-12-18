ef6f0991ff0e5c01500515933eccd74d6bf22636
Hinesh Amin <hineshamin01@gmail.com>
2018-12-17 21:20:35 -0800

    Fixing contact page spacing

--- src/Contact.js
+++ src/Contact.js
@@ -12,59 +12,43 @@ class Contact extends Component {
           <WorkLines />
         </MtSvgLines>
         <div className="show">
-          <div className="row">
+          <div className="row mb-3">
             <div className="col-1" />
             <div className="col-10">
               <h1 className="active contact">Contact me</h1>
             </div>
           </div>
           <div className="row">
-            <div className="col-1" />
-            <div className="col-10">
+            <div className="col-10 offset-1">
               <a
                 href="https://github.com/hineshamin"
-                className="btn btn-primary github"
+                className="btn btn-primary github mr-3"
                 target="_blank"
               >
                 <h3 className="text-white mt-2">
                   <i className="fab fa-github" /> Github
                 </h3>
               </a>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-1" />
-            <div className="col-10">
               <a
                 href="https://www.linkedin.com/in/hinesh-amin-9084a094/"
-                className="btn btn-primary linkedin"
+                className="btn btn-primary linkedin mr-3"
                 target="_blank"
               >
                 <h3 className="text-white mt-2">
                   <i className="fab fa-linkedin" /> LinkedIn
                 </h3>
               </a>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-1" />
-            <div className="col-10">
               <a
                 href="mailto:hineshamin01@gmail.com"
-                className="btn btn-primary email"
+                className="btn btn-primary email mr-3"
               >
                 <h3 className="text-white mt-2">
                   <i className="fas fa-envelope" /> Email
                 </h3>
               </a>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-1" />
-            <div className="col-10">
               <a
                 href={resume}
-                className="btn btn-primary resume"
+                className="btn btn-primary resume mr-3"
                 target="_blank"
               >
                 <h3 className="text-white mt-2">
